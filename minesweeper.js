198e8fd482793562e35ea2a7ae7f570f2967ac61
Edi Rose <edirose1998@gmail.com>
2018-01-03 15:49:21 +1300

    mod

--- minesweeper.js
+++ minesweeper.js
@@ -20,68 +20,74 @@ board.cells= [
 function startGame () {
   // Don't remove this function call: it makes the game work!
    document.addEventListener("click", checkForWin);
+   document.addEventListener("control", checkForWin);
   //
    for (var i = 0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
    }
   lib.initBoard()
 }
-
-// Define this function to look for a win condition:
-//
-// 1. Are all of the cells that are NOT mines visible?
-// 2. Are all of the mines marked?
-// function checkForWin () {
-//
-//   var win = false;
-//
-//   for (var i = 0; i <board.cells.length; i++) {
-//
-//     if (board.cells[i].hidden === true){
-//       if ((board.cells[i].isMine === true) && (board.cells[i].isMarked === true)){
-//             set(win=true);
-//           }
-//     }
-//      if ((board.cells[i].isMine)&&(board.cells[i].hidden=== false)) {
-//        set(win=true);
-//      }
-//      if (win=true) {
-//        lib.displayMessage('You win!');
-//      }
-//      else {
-//        return;
-//      }
-// }
-// }
 function checkForWin () {
-
-var markedMines = 0;
- for(i = 0; i< board.cells.length; i++) {
-
-   var isMine = board.cells[i].isMine;
-   var isMarked = board.cells[i].isMarked;
-
-   if ((isMine === true) && (isMarked === true)) {
-     markedMines++;
+   for (var i = 0; i < board.cells.length; i++) {
+     if (board.cells[i].hidden) {
+       if (!board.cells[i].isMine) {
+         return
+       }
+     }
+     if (board.cells[i].isMine) {
+       if (!board.cells[i].isMarked) {
+         return
+       }
+     }
    }
  }
+// function checkForWin () {
+//  //var cell = board.cells[i];
+//  for (var i = 0; i < board.cells.length; i++) {
+//    if ((board.cells.isMine) && (!board.cells.isMarked)) {
+//      return;
+//    }
+//    if ((!board.cells.isMine) && (board.cells.isMarked)) {
+//      return;
+//    } else if (!board.cells.hidden) {
+//      lib.displayMessage('You win!')
+//    } else {
+//      return;
+//    }
+//  }
+// }
 
- var uncoveredSafe= 0;
+//REAL SOLUTION
+//  function checkForWin () {
+//
+// var markedMines = 0;
+//  for(i = 0; i< board.cells.length; i++) {
+//
+//    var isMine = board.cells[i].isMine;
+//    var isMarked = board.cells[i].isMarked;
+//
+//    if ((isMine === true) && (isMarked === true)) {
+//      markedMines++;
+//    }
+//  }
+//
+//  var uncoveredSafe= 0;
+//
+//
+//  for ( var i = 0; i <board.cells.length; i++) {
+//
+//    var isMine = board.cells[i].isMine;
+//    var isHidden = board.cells[i].hidden;
+//
+//        if ((isMine === false) && (isHidden === false)){
+//          uncoveredSafe++;
+//   }
+//  }
+//  if ((markedMines === 3) || (uncoveredSafe===6)) {
+//    lib.displayMessage('You win!');
+// }
+// }
 
-
- for ( var i = 0; i <board.cells.length; i++) {
-
-   var isMine = board.cells[i].isMine;
-   var isHidden = board.cells[i].hidden;
-
-       if ((isMine === false) && (isHidden === false)){
-         uncoveredSafe++;
-  }
- }
- if ((markedMines === 3) || (uncoveredSafe===6)) {
-   lib.displayMessage('You win!');
-}
-}
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
